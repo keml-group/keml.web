67157f21b3b3904fc70f3eb92b737e0e8555c123
Susanne Göbel <goebel@uni-koblenz.de>
2024-06-20 11:03:42 +0200

    Applying example styles to nodes, using an own component as node did not work

--- src/editor/central-window/central-window.component.ts
+++ src/editor/central-window/central-window.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import {FlowShapeModel} from "@syncfusion/ej2-angular-diagrams";
 
 @Component({
   selector: 'central-window',
@@ -6,5 +7,10 @@ import { Component } from '@angular/core';
   styleUrl: './central-window.component.css'
 })
 export class CentralWindowComponent {
-
+  public terminator:FlowShapeModel =  { type: 'Flow', shape: 'Terminator'};
+  public borderColor = 'orange';
+  public borderWidth = 10;
+  public style = {fill:'red',strokeColor:'green',strokeWidth:5,strokeDashArray:'2 2'};
+  ngOnInit(): void {
+  }
 }
--- src/editor/editor.component.ts
+++ src/editor/editor.component.ts
@@ -1,10 +1,19 @@
-import {Component, } from '@angular/core';
+import {Component, ViewChild, } from '@angular/core';
+import {BpmnDiagramsService, BpmnShapeModel, DiagramComponent, FlowShapeModel} from "@syncfusion/ej2-angular-diagrams";
 
 @Component({
   selector: 'keml-editor',
   templateUrl: './editor.component.html',
-  styleUrl: './editor.component.css'
+  styleUrl: './editor.component.css',
+  providers: [BpmnDiagramsService]
 })
 export class EditorComponent {
 
+  public terminator:FlowShapeModel =  { type: 'Flow', shape: 'Terminator'};
+  public preKnowledge: BpmnShapeModel = { type: 'Bpmn', shape: 'DataSource'};
+  public borderColor = 'orange';
+  public borderWidth = 10;
+  public style = {fill:'red',strokeColor:'green',strokeWidth:5,strokeDashArray:'2 2'};
+  ngOnInit(): void {
+  }
 }
