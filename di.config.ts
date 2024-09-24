a5143ba76582bd029745da5a34beffdc77fec5d4
Susanne Göbel <goebel@uni-koblenz.de>
2024-09-24 08:52:30 +0200

    Added di config as stated on sprotty page, still app crashes

---
+++ di.config.ts
@@ -0,0 +1,23 @@
+import { Container, ContainerModule } from 'inversify';
+import { configureModelElement, configureViewerOptions, loadDefaultModules, LocalModelSource, PolylineEdgeView, SEdgeImpl, SGraphImpl, SGraphView, SNodeImpl, TYPES } from 'sprotty';
+
+export const createContainer = (containerId: string) => {
+  const myModule = new ContainerModule((bind, unbind, isBound, rebind) => {
+    bind(TYPES.ModelSource).to(LocalModelSource).inSingletonScope();
+
+    const context = { bind, unbind, isBound, rebind };
+    configureModelElement(context, 'graph', SGraphImpl, SGraphView);
+    //configureModelElement(context, 'task', SNodeImpl, TaskNodeView);
+    configureModelElement(context, 'edge', SEdgeImpl, PolylineEdgeView);
+
+    configureViewerOptions(context, {
+      needsClientLayout: false,
+      baseDiv: containerId
+    });
+  });
+
+  const container = new Container();
+  loadDefaultModules(container);
+  container.load(myModule);
+  return container;
+};
