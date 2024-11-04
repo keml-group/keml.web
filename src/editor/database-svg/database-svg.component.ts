3d8545a75c123f6c8cb4f32d92a821d68be9126d
Susanne Göbel <goebel@uni-koblenz.de>
2024-11-04 16:37:04 +0100

    Using parametrized database icon, however still scaled instead of just makng the icon adjust itself

--- src/editor/database-svg/database-svg.component.ts
+++ src/editor/database-svg/database-svg.component.ts
@@ -10,7 +10,21 @@ export class DatabaseSvgComponent {
   @Input() stroke: string = 'black'
   @Input() strokewidth: number = 0.2;
   @Input() fill: string = 'grey';
-  @Input() w: number = 300;
-  @Input() h: number = 150;
+  @Input() w: number = 48;
+  @Input() h: number = 15;
+
+
+  computePath(): string {
+    const w = this.w;
+    const h = this.h;
+    const h1 = w/6;
+    const hges = h1+h;
+
+    return 'M 0 '+h1+' C 1 '+(2*h1)+' '+(w-1)+' '+(2*h1)+' '+w+' '+h1+'\n'
+      +' C '+(w-1)+' 0 1 '+' 0 0 '+h1+'\n'
+      +'v '+hges+'\n'
+      +' C 1 '+(2*h1+hges)+' '+(w-1)+' '+(2*h1+hges)+' '+w+' '+(h1+hges)+'\n'
+      +'v -'+hges;
+  }
 
 }
