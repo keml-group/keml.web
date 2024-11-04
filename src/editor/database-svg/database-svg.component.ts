0c0d1567045aee3bece3eecd9eb56be550f1a4e9
Susanne Göbel <goebel@uni-koblenz.de>
2024-11-04 13:22:25 +0100

    Allowing to control stroke and fill color as parameters

--- src/editor/database-svg/database-svg.component.ts
+++ src/editor/database-svg/database-svg.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import {Component, Input} from '@angular/core';
 
 @Component({
   selector: '[database]',
@@ -6,5 +6,8 @@ import { Component } from '@angular/core';
   styleUrl: './database-svg.component.css'
 })
 export class DatabaseSvgComponent {
+
+  @Input() stroke: string = 'black'
+  @Input() fill: string = 'grey';
 
 }
