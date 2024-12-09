5838da714ffe7436dea316e19deb94dbc6c90bfb
Susanne Göbel <goebel@uni-koblenz.de>
2024-12-09 11:47:54 +0100

    Removing unnecessary sourcePos attribute and all relevant code, OnInit and OnCHanges

--- src/editor/new-info/new-info.component.ts
+++ src/editor/new-info/new-info.component.ts
@@ -1,31 +1,13 @@
-import {Component, Input, OnChanges, OnInit, SimpleChanges} from '@angular/core';
+import {Component, Input} from '@angular/core';
 import {InfoComponent} from "../info/info.component";
 import {NewInformation} from "../../shared/models/keml/msg-info";
-import {BoundingBox} from "../../shared/models/graphical/bounding-box";
-import {LayoutHelper} from "../../shared/utility/layout-helper";
 
 @Component({
   selector: '[newInfoG]',
   templateUrl: './new-info.component.svg',
   styleUrl: './new-info.component.css'
 })
-export class NewInfoComponent extends InfoComponent implements OnInit, OnChanges {
+export class NewInfoComponent extends InfoComponent {
   @Input() override info!: NewInformation;
-
-  sourcePos: BoundingBox;
-
-  constructor() {
-    super();
-    this.sourcePos = new BoundingBox(0, 0, 10, 10);
-  }
-
-  ngOnInit(): void {
-    this.sourcePos.y = (LayoutHelper.computeMessageY(this.info.source.timing) );
-  }
-
-  // todo necessary?
-  ngOnChanges(_: SimpleChanges): void {
-    this.sourcePos.y = (LayoutHelper.computeMessageY(this.info.source.timing) );
-  }
 
 }
