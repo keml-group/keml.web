8fbac52dce6022b651f181097624e3dd6369d5fc
Susanne Göbel <goebel@uni-koblenz.de>
2024-12-29 10:22:02 +0100

    Own modal to choose info from - however, msg is not adapted correctly yet and data is not in there yet

--- src/editor/info-choice/info-choice.component.ts
+++ src/editor/info-choice/info-choice.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import {Component, EventEmitter, Input, Output, TemplateRef, ViewChild} from '@angular/core';
+import {Information, Preknowledge, ReceiveMessage} from "../../shared/models/keml/msg-info";
+import {MatDialog} from "@angular/material/dialog";
 
 @Component({
   selector: 'info-choice',
@@ -6,5 +8,25 @@ import { Component } from '@angular/core';
   styleUrl: './info-choice.component.css'
 })
 export class InfoChoiceComponent {
+
+  constructor(
+    private dialog: MatDialog,
+  ) {}
+
+  //graphical component to pick an info, hence it needs all messages and all preknowledge as input:
+  @Input() preknowledges!: Preknowledge[];
+  @Input() receives!: ReceiveMessage[];
+
+  @Output() choice = new EventEmitter<Information>();
+
+  @ViewChild('chooseinfo') modalRef!: TemplateRef<any>;
+
+  openChoice() {
+    this.dialog.open(this.modalRef,{})
+  }
+
+  choose(info: Information) {
+    this.choice.emit(info);
+  }
 
 }
