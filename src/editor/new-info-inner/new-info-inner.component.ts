f5f151d7c2d16783c92ab4ff3139624f73523fed
Susanne Göbel <goebel@uni-koblenz.de>
2025-01-02 16:52:12 +0100

    Inner info component with a different picture for new info and preknowledge. It is now linked on the info choice so that the current info is always visible

--- src/editor/new-info-inner/new-info-inner.component.ts
+++ src/editor/new-info-inner/new-info-inner.component.ts
@@ -1,27 +1,36 @@
-import {Component, Input} from '@angular/core';
-import {NewInformation} from "../../shared/models/keml/msg-info";
+import {Component, Input, OnChanges, OnInit, SimpleChanges} from '@angular/core';
+import {Information, NewInformation} from "../../shared/models/keml/msg-info";
 
 @Component({
-  selector: '[new-info-inner]',
+  selector: '[info-inner]',
   templateUrl: './new-info-inner.component.svg',
   styleUrl: './new-info-inner.component.css'
 })
 // todo we cannot use it on new info currently, it leads to bounding box errors on arrows
-export class NewInfoInnerComponent {
+export class NewInfoInnerComponent implements OnInit, OnChanges {
 
-  @Input() info!: NewInformation
+  @Input() info!: Information
+  isNew = true;
+  color = '#ffff99'
 
-  computeTextColor():string {
-    const name = this.getConvPartnerName();
-    return name == 'LLM'? '#ccffff' : '#ffff99'
+  ngOnInit() {
+    this.determineTypeAndColor()
   }
 
-  // todo later use color here, then this is main method
-  getConvPartnerName(): string {
+  ngOnChanges(_: SimpleChanges) {
+    this.determineTypeAndColor()
+  }
+
+  private determineTypeAndColor() {
     const source = (this.info as NewInformation).source
-    if(source ) {
-      return source.counterPart.name; //todo use color
-    } else return 'author'; //give colors here already, currently we use the template vehicle
+    if (source) {
+      this.isNew = true
+      this.color = source.counterPart.name == 'LLM'? '#ccffff' : '#ffff99'
+      //todo maybe use different conversationPartner colors?
+    } else {
+      this.isNew = false
+      this.color = '#ffff99'
+    }
   }
 
 }
