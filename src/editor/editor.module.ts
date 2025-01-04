c014e06287fbc5865eca2e9cfcede90c15f870e5
Susanne Göbel <goebel@uni-koblenz.de>
2025-01-04 15:29:53 +0100

    Refactoring: moving all helper components into an own folder (helper)

--- src/editor/editor.module.ts
+++ src/editor/editor.module.ts
@@ -5,23 +5,23 @@ import {MatToolbar} from "@angular/material/toolbar";
 import {MatIcon} from "@angular/material/icon";
 import {ConversationPartnerComponent} from "./cp/conversation-partner.component";
 import { FormsModule } from '@angular/forms';
-import {PersonSvgComponent} from "./person-svg/person-svg.component";
+import {PersonSvgComponent} from "./helper/person-svg/person-svg.component";
 import {AuthorComponent} from "./author/author.component";
 import {MsgComponent} from "./msg/msg.component";
 import {InfoComponent} from "./info/info.component";
-import {DatabaseSvgComponent} from "./database-svg/database-svg.component";
+import {DatabaseSvgComponent} from "./helper/database-svg/database-svg.component";
 import {MsgDetailsComponent} from "./details/msg-details/msg-details.component";
 import {ConversationPartnerDetailsComponent} from "./details/cp-details/cp-details.component";
 import {InfoDetailsComponent} from "./details/info-details/info-details.component";
-import {IsInstrSvgComponent} from "./is-instr-svg/is-instr-svg.component";
+import {IsInstrSvgComponent} from "./helper/is-instr-svg/is-instr-svg.component";
 import {PreknowledgeComponent} from "./preknowledge/preknowledge.component";
-import {TextAreaSvgComponent} from "./text-area-svg/text-area-svg.component";
-import {ArrowSvgComponent} from "./arrow-svg/arrow-svg.component";
+import {TextAreaSvgComponent} from "./helper/text-area-svg/text-area-svg.component";
+import {ArrowSvgComponent} from "./helper/arrow-svg/arrow-svg.component";
 import {NewInfoComponent} from "./new-info/new-info.component";
 import {InformationLinkComponent} from "./information-link/information-link.component";
 import {InformationLinkDetailsComponent} from "./details/information-link-details/information-link-details.component";
-import {InfoChoiceComponent} from "./info-choice/info-choice.component";
-import {InfoInnerComponent} from "./info-inner/info-inner.component";
+import {InfoChoiceComponent} from "./helper/info-choice/info-choice.component";
+import {InfoInnerComponent} from "./helper/info-inner/info-inner.component";
 
 
 @NgModule({
