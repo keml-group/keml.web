a3ac292cb93ba4d159c6f6b023f4e66045f04258
Susanne Göbel <goebel@uni-koblenz.de>
2025-01-05 20:58:22 +0100

    Add repetition arrows on msg, therefore we need to make the receive explicit

--- src/editor/msg/msg.component.ts
+++ src/editor/msg/msg.component.ts
@@ -1,22 +1,34 @@
-import {Component, EventEmitter, Input, Output} from '@angular/core';
-import {Message} from "../../shared/models/keml/msg-info";
+import {Component, EventEmitter, Input, OnInit, Output} from '@angular/core';
+import {Message, SendMessage} from "../../shared/models/keml/msg-info";
 import {ReceiveMessage} from "../../shared/models/keml/msg-info";
 import {Information} from "../../shared/models/keml/msg-info";
 import {NewInformation} from "../../shared/models/keml/msg-info";
 import {LayoutHelper} from "../../shared/utility/layout-helper";
+import {ArrowType} from "../../shared/models/graphical/arrow-heads";
 
 @Component({
   selector: '[msgG]',
   templateUrl: './msg.component.svg',
   styleUrl: './msg.component.css'
 })
-export class MsgComponent {
+export class MsgComponent implements OnInit{
   @Input() msg!: Message;
   @Input() showInfos = true;
   @Output() chooseMsg: EventEmitter<Message> = new EventEmitter();
   @Output() chooseInfo = new EventEmitter<Information>();
 
+  receiveMsg?: ReceiveMessage;
+  sendMsg?: SendMessage;
+
   constructor() { }
+
+  ngOnInit() {
+    if (this.msg.isSend()) {
+      this.sendMsg = (this.msg as SendMessage)
+    } else {
+      this.receiveMsg = (this.msg as ReceiveMessage)
+    }
+  }
 
   computeY(): number {
     return LayoutHelper.computeMessageY(this.msg.timing);
@@ -37,4 +49,6 @@ export class MsgComponent {
   clickInfo(info: Information) {
     this.chooseInfo.emit(info);
   }
+
+  protected readonly ArrowType = ArrowType;
 }
