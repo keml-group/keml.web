7335630eecba6ea3b930603b36bd73921160da51
Susanne Göbel <goebel@uni-koblenz.de>
2025-01-08 15:27:13 +0100

    Use ngAfterViewInti on msgs to trigger change on arrows

--- src/editor/msg/msg.component.ts
+++ src/editor/msg/msg.component.ts
@@ -1,17 +1,18 @@
-import {Component, EventEmitter, Input, OnInit, Output} from '@angular/core';
+import {AfterViewInit, Component, EventEmitter, Input, OnInit, Output} from '@angular/core';
 import {Message, SendMessage} from "../../shared/models/keml/msg-info";
 import {ReceiveMessage} from "../../shared/models/keml/msg-info";
 import {Information} from "../../shared/models/keml/msg-info";
 import {NewInformation} from "../../shared/models/keml/msg-info";
 import {LayoutHelper} from "../../shared/utility/layout-helper";
 import {ArrowType} from "../../shared/models/graphical/arrow-heads";
+import {SVGAccessService} from "../../shared/services/svg-access.service";
 
 @Component({
   selector: '[msgG]',
   templateUrl: './msg.component.svg',
   styleUrl: './msg.component.css'
 })
-export class MsgComponent implements OnInit{
+export class MsgComponent implements OnInit, AfterViewInit {
   @Input() msg!: Message;
   @Input() showInfos = true;
   @Output() chooseMsg: EventEmitter<Message> = new EventEmitter();
@@ -20,7 +21,7 @@ export class MsgComponent implements OnInit{
   receiveMsg?: ReceiveMessage;
   sendMsg?: SendMessage;
 
-  constructor() { }
+  constructor(private svgAccessService: SVGAccessService) { }
 
   ngOnInit() {
     if (this.msg.isSend()) {
@@ -28,6 +29,10 @@ export class MsgComponent implements OnInit{
     } else {
       this.receiveMsg = (this.msg as ReceiveMessage)
     }
+  }
+
+  ngAfterViewInit() {
+    this.svgAccessService.notifyPositionChange(this.msg.gId)
   }
 
   computeY(): number {
