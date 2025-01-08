f907fd1f1c0289099b2735ffadabbd2e3651dbda
Susanne Göbel <goebel@uni-koblenz.de>
2025-01-08 14:05:03 +0100

    CHange component interaction to go from arrow between to arrow svg: svg now always expects two positions and recomputes on their change

--- src/editor/helper/arrow-between-elems/arrow-between-elems.component.ts
+++ src/editor/helper/arrow-between-elems/arrow-between-elems.component.ts
@@ -1,16 +1,27 @@
-import {Component, Input, OnInit} from '@angular/core';
+import {
+  AfterViewInit,
+  ChangeDetectorRef,
+  Component, ElementRef,
+  Input,
+  OnChanges,
+  OnInit,
+  SimpleChanges,
+  ViewChild
+} from '@angular/core';
 import {BoundingBox} from "../../../shared/models/graphical/bounding-box";
 import {ArrowType} from "../../../shared/models/graphical/arrow-heads";
+import {PositionHelper} from "../../../shared/models/graphical/position-helper";
+import {SVGAccessService} from "../../../shared/services/svg-access.service";
 
 @Component({
   selector: '[arrowElems]',
   templateUrl: './arrow-between-elems.component.svg',
   styleUrl: './arrow-between-elems.component.css'
 })
-export class ArrowBetweenElemsComponent implements OnInit {
+export class ArrowBetweenElemsComponent implements OnInit, AfterViewInit, OnChanges {
 
-  @Input() startBox?: BoundingBox;
-  @Input() endBox?: BoundingBox;
+  //@Input() startBox?: BoundingBox;
+  //@Input() endBox?: BoundingBox;
   @Input() arrowType: ArrowType = ArrowType.STANDARD;
   @Input() breaks: BoundingBox[] = [];
   @Input() text?: string;
@@ -21,7 +32,48 @@ export class ArrowBetweenElemsComponent implements OnInit {
   start?: BoundingBox;
   end?: BoundingBox;
 
+  positioned= false;
+  @ViewChild('arrow') node!: ElementRef<SVGGraphicsElement>;
+
+  //idea: compute the two input positions as relative to the current elem
+  constructor(
+    private svgAccessService: SVGAccessService,
+    private cdr: ChangeDetectorRef) {
+  }
+
   ngOnInit() {
+  }
+
+  ngAfterViewInit() {
+    console.log("Init done, start is "+this.startId)
+    this.computePositionsByIds()
+  }
+
+  ngOnChanges(changes: SimpleChanges) {
+    if(this.positioned) {
+      this.computePositionsByIds()
+    }
+  }
+
+  private computePositionsByIds() {
+    let startElem: SVGGraphicsElement | undefined = this.svgAccessService.getElemById(this.startId!)
+    let endElem: SVGGraphicsElement | undefined = this.svgAccessService.getElemById(this.endId!)
+    if (startElem && endElem) {
+      let startAbs = PositionHelper.absolutePosition(startElem!)
+      let endAbs = PositionHelper.absolutePosition(endElem!)
+      console.log('pos of start '+ this.startId + ' is ' + startAbs)
+      console.log('pos of end '+ this.endId + ' is ' + endAbs)
+      if (this.node?.nativeElement){
+        let el = this.node.nativeElement as SVGGraphicsElement
+        PositionHelper.makeRelativeToElem(startAbs, el)
+        PositionHelper.makeRelativeToElem(endAbs, el)
+        this.start = startAbs
+        this.end = endAbs
+      } else
+        console.log('No native element yet')
+    } else {
+      console.log('No elems yet')
+    }
   }
 
 }
