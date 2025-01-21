ba74d2d8210565e2bc0c734b3df9de5a1cf85f9d
Susanne Göbel <goebel@uni-koblenz.de>
2025-01-21 04:41:16 +0100

    Suppress schema errors, add input and generate extra test for arrow type

--- src/editor/helper/arrow-svg/arrow-svg.component.spec.ts
+++ src/editor/helper/arrow-svg/arrow-svg.component.spec.ts
@@ -1,6 +1,9 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+import {ComponentFixture, TestBed} from '@angular/core/testing';
 
-import { ArrowSvgComponent } from './arrow-svg.component';
+import {ArrowSvgComponent} from './arrow-svg.component';
+import {NO_ERRORS_SCHEMA} from "@angular/core";
+import {BoundingBox} from "../../../shared/models/graphical/bounding-box";
+import {ArrowHead, CustomArrowType} from "../../../shared/models/graphical/arrow-heads";
 
 describe('ArrowSvgComponent', () => {
   let component: ArrowSvgComponent;
@@ -8,16 +11,30 @@ describe('ArrowSvgComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ArrowSvgComponent]
+      declarations: [ArrowSvgComponent],
+      schemas: [NO_ERRORS_SCHEMA],
     })
     .compileComponents();
 
+    let start = new BoundingBox(50, -20, 200, 80)
+    let end = new BoundingBox(80, 20, 100, 200)
+
     fixture = TestBed.createComponent(ArrowSvgComponent);
     component = fixture.componentInstance;
+    component.start = start
+    component.end = end
     fixture.detectChanges();
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it('should set right arrowHead and dashed (when positioned)', () => {
+    component.positioned = true
+    component.arrowType = CustomArrowType.DASHED
+    component.ngOnChanges()
+    expect(component.dashed).toEqual([5])
+    expect(component.endType).toEqual(ArrowHead.POINTER)
+  })
 });
