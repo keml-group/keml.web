ae2ebbe5257c79c901f166941cd4210817b03233
Susanne Göbel <goebel@uni-koblenz.de>
2025-01-25 04:26:22 +0100

    Add test for new info

--- src/shared/models/keml/msg-info.spec.ts
+++ src/shared/models/keml/msg-info.spec.ts
@@ -1,7 +1,10 @@
-import {Preknowledge} from "./msg-info";
-import {PreknowledgeJson} from "./json/knowledge-models";
+import {NewInformation, Preknowledge, ReceiveMessage} from "./msg-info";
+import {NewInformationJson, PreknowledgeJson} from "./json/knowledge-models";
+import {ConversationPartner} from "./conversation-partner";
+import {Author} from "./author";
 
 describe('Msg-Info (models)', () => {
+
   it('should serialize preknowledge', () => {
     let preknowledge = new Preknowledge()
     let preknowledgeJson : PreknowledgeJson = {
@@ -19,5 +22,28 @@ describe('Msg-Info (models)', () => {
       targetedBy: []
     }
     expect(preknowledge.toJson()).toEqual(preknowledgeJson);
+  });
+
+  it('should serialize newInfo', () => {
+    let author = new Author()
+    let cp = new ConversationPartner()
+    let msg = new ReceiveMessage(cp, 1, "receiveContent")
+    let newInfo = new NewInformation(msg, 'New Info')
+    let newInfoJson: NewInformationJson = {
+      source: msg.getRef(),
+      causes: [],
+      currentTrust: 0.5,
+      eClass: NewInformation.eClass,
+      feltTrustAfterwards: undefined,
+      feltTrustImmediately: undefined,
+      initialTrust: 0.5,
+      isInstruction: false,
+      isUsedOn: [],
+      message: 'New Info',
+      position: {x: 0, y: 0, w: 5, h: 5},
+      repeatedBy: [],
+      targetedBy: []
+    }
+    expect(newInfo.toJson()).toEqual(newInfoJson);
   });
 });
