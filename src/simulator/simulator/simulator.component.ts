0be3cdd0c803eef4d25ad80e1cebf72ad68fdd13
Susanne Göbel <goebel@uni-koblenz.de>
2025-01-28 13:50:09 +0100

    Close button and some css

--- src/simulator/simulator/simulator.component.ts
+++ src/simulator/simulator/simulator.component.ts
@@ -1,10 +1,13 @@
 import {Component, Input} from '@angular/core';
 import {Conversation} from "../../shared/models/keml/conversation";
+import {MatIcon} from "@angular/material/icon";
 
 @Component({
   selector: 'app-simulator',
   standalone: true,
-  imports: [],
+  imports: [
+    MatIcon
+  ],
   templateUrl: './simulator.component.html',
   styleUrl: './simulator.component.css'
 })
