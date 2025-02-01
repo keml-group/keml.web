3cd542229537a6421a267fe58546e3c74ac32332
Susanne Göbel <goebel@uni-koblenz.de>
2025-02-01 06:39:09 +0100

    Step2 of auto migration to standalone: removes editor module. Manually added EditorComponent as import on app; also manually remove simulator module

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import {EditorModule} from "../editor/editor.module";
+import {EditorComponent} from "../editor/editor.component";
+
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, EditorModule],
+  imports: [RouterOutlet, EditorComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
