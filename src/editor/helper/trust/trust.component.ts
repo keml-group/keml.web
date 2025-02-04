1c3a63209162fe948c6400c48373ffc18a419be6
Susanne Göbel <goebel@uni-koblenz.de>
2025-02-04 05:23:53 +0100

    Enable trust color computation for red and green colors, also display trust vertically

--- src/editor/helper/trust/trust.component.ts
+++ src/editor/helper/trust/trust.component.ts
@@ -30,7 +30,26 @@ export class TrustComponent implements OnInit, OnChanges {
   }
 
   computeColor() {
-    return '#666666' // todo
+    if (!this.trust ) {
+      return '#666666' // todo
+    }
+    if (this.trust < 0) {
+      return this.useRed()
+    } else {
+      return this.useGreen()
+    }
+  }
+
+  useRed() {
+    let factor = 1- this.trust
+    let green = (factor * 160).toString(16)
+    console.log(green)
+    return '#ff'+green +'00'
+  }
+
+  useGreen() {
+    let red = '80' // todo
+    return '#'+red+'ff00'
   }
 
 }
