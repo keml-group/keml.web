2ea603ab345bb8be10aa37ef3fcecce08c103ef3
Susanne Göbel <goebel@uni-koblenz.de>
2025-02-13 15:40:33 +0100

    test for link destruction that shows the problem

--- src/shared/models/keml/msg-info.spec.ts
+++ src/shared/models/keml/msg-info.spec.ts
@@ -144,4 +144,13 @@ describe('Msg-Info (models)', () => {
     expect(infoLink_pre_pre_2.toJson()).toEqual(infoLink_pre_pre_2_Json);
   });
 
+  it('should delete an info link completely', () => {
+    let p0 = new Preknowledge('p0')
+    let p1 = new Preknowledge('p1')
+    let link = new InformationLink(p1, p0, InformationLinkType.SUPPORT)
+
+    expect(p0.targetedBy.length).toEqual(1)
+    link.destruct()
+    expect(p0.targetedBy.length).toEqual(0)
+  })
 });
