b6bfad282b0f799b0d4a1934fc94b92614c1b606
Susanne Göbel <goebel@uni-koblenz.de>
2025-02-19 10:12:26 +0100

    Add info-trust-details, open it from simulation service

--- src/app/features/simulator/services/simulation.service.ts
+++ src/app/features/simulator/services/simulation.service.ts
@@ -3,6 +3,7 @@ import {Information} from "@app/shared/keml/models/core/msg-info";
 import {Conversation} from "@app/shared/keml/models/core/conversation";
 import {SimulatorComponent} from "@app/features/simulator/components/simulator/simulator.component";
 import {MatDialog} from "@angular/material/dialog";
+import {InfoTrustDetailsComponent} from "@app/features/simulator/components/info-trust-details/info-trust-details.component";
 
 @Injectable({
   providedIn: 'root'
@@ -26,6 +27,14 @@ export class SimulationService {
   }
 
   openInfoTrusts(info: Information) {
-    console.log("Open trust view on info")
+    const dialogRef = this.dialog.open(
+      InfoTrustDetailsComponent,
+      {
+        width: '80vw',
+        maxWidth: '100vw', //otherwise it is 80, see https://stackoverflow.com/questions/46034619/angular-material-2-how-to-overwrite-dialog-max-width-to-100vw
+        height: '100vh'
+      }
+    )
+    dialogRef.componentInstance.info = info
   }
 }
