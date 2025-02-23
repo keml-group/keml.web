ef27a055ffa941587a520fad92a616b0ebe8edde
Susanne Göbel <goebel@uni-koblenz.de>
2025-02-23 08:43:32 +0100

    Workaround: map only works after keyvalue pipe, but then not the original value gets changed but some new version in the pipe. Hence, we have two lists and trigger the changes accordingly.

--- src/app/features/simulator/components/simulation-input-details/simulation-input-details.component.ts
+++ src/app/features/simulator/components/simulation-input-details/simulation-input-details.component.ts
@@ -1,10 +1,10 @@
-import {Component, Input} from '@angular/core';
+import {Component, Input, OnInit} from '@angular/core';
 import {MatDialogRef} from "@angular/material/dialog";
 import {MatIcon} from "@angular/material/icon";
 import {FormsModule, ReactiveFormsModule} from "@angular/forms";
 import {SimulationInputs} from "@app/features/simulator/models/simulation-inputs";
-import {ConversationPartner} from "@app/shared/keml/models/core/conversation-partner";
 import {KeyValuePipe, NgForOf} from "@angular/common";
+import {ConversationPartner} from "@app/shared/keml/models/core/conversation-partner";
 
 @Component({
   selector: 'simulation-input-details',
@@ -19,13 +19,24 @@ import {KeyValuePipe, NgForOf} from "@angular/common";
   templateUrl: './simulation-input-details.component.html',
   styleUrl: './simulation-input-details.component.css'
 })
-export class SimulationInputDetails {
+export class SimulationInputDetails implements OnInit {
 
   @Input() simulationInputs!: SimulationInputs
+  partnerList: ConversationPartner[] = []
+  defaultList: (number|undefined)[] = []
 
   constructor(
     private dialogRef: MatDialogRef<SimulationInputDetails>,
   ) {}
+
+  ngOnInit() {
+    this.partnerList = Array.from(this.simulationInputs.defaultsPerCp.keys())
+    this.defaultList = Array.from(this.simulationInputs.defaultsPerCp.values())
+  }
+
+  useListsForSimInputs(cp: ConversationPartner, val: (number| undefined)) {
+    this.simulationInputs.defaultsPerCp.set(cp, val)
+  }
 
   closeMe() {
     this.dialogRef.close();
