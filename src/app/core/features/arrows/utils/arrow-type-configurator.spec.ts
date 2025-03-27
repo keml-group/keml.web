3dded6d6f8e7fbcc9bd9cb7f87d632015d3d55fd
Susanne Göbel <goebel@uni-koblenz.de>
2025-03-27 19:39:01 +0100

    Add test for new configurator

--- src/app/core/features/arrows/utils/arrow-type-configurator.spec.ts
+++ src/app/core/features/arrows/utils/arrow-type-configurator.spec.ts
@@ -1,7 +1,32 @@
-import { ArrowTypeConfigurator } from './arrow-type-configurator';
+import {ArrowTypeConfigurator} from './arrow-type-configurator';
+import {ArrowHead, ArrowType} from "@app/core/features/arrows/models/arrow-heads";
 
 describe('ArrowTypeConfigurator', () => {
   it('should create an instance', () => {
     expect(new ArrowTypeConfigurator()).toBeTruthy();
   });
+
+  it('should set right arrowHead, color and dashed for None', () => {
+    expect(ArrowTypeConfigurator.styleArrow()).toEqual({
+      dashed: [0],
+      color: 'black',
+      endPointer: ArrowHead.POINTER,
+    })
+  })
+
+  it('should set right arrowHead, color and dashed for DASHED', () => {
+    expect(ArrowTypeConfigurator.styleArrow(ArrowType.DASHED)).toEqual({
+      dashed: [5],
+      color: 'black',
+      endPointer: ArrowHead.POINTER,
+    })
+  })
+
+  it('should set right arrowHead, color and dashed for STRONG_ATTACK', () => {
+    expect(ArrowTypeConfigurator.styleArrow(ArrowType.STRONG_ATTACK)).toEqual({
+      dashed: [0],
+      color: 'red',
+      endPointer: ArrowHead.ATTACK,
+    })
+  })
 });
