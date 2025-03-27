6a218c5f8ffa05828566f5e3cdb3405caba1060a
Susanne Göbel <goebel@uni-koblenz.de>
2025-03-27 16:05:11 +0100

    Remove unused class about arrow colors

--- src/app/core/features/arrows/models/arrow-heads.ts
+++ src/app/core/features/arrows/models/arrow-heads.ts
@@ -7,18 +7,6 @@ export enum ArrowHead {
   SUPPLEMENT = 'SUPPLEMENT',
 }
 
-export class ColorFromArrowHead {
-  static getColor(arrowHead: ArrowHead): string {
-    switch (arrowHead) {
-      case ArrowHead.ATTACK: return 'red';
-      case ArrowHead.SUPPORT: return 'green';
-      case ArrowHead.SUPPLEMENT:
-        case ArrowHead.POINTER:
-          return 'black';
-    }
-  }
-}
-
 export enum CustomArrowType {
   STANDARD = 'STANDARD',
   DASHED = 'DASHED',
