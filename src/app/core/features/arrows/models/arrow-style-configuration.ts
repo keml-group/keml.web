fbb9f3d9ffa2d6424963a35bfa537e07716ff598
Susanne Göbel <goebel@uni-koblenz.de>
2025-03-28 11:21:02 +0100

    Change marker on config to simple string that is then used in a url reference. On the marker definition and when setting values for the config, the enum can still be used

--- src/app/core/features/arrows/models/arrow-style-configuration.ts
+++ src/app/core/features/arrows/models/arrow-style-configuration.ts
@@ -1,11 +1,9 @@
-import {ArrowHead} from "@app/core/features/arrows/models/arrow-heads";
-
 export interface ArrowStyleConfiguration {
   color: string;
   dashed: number[];
   //style?: string
 
-  //startPointer?: SVGMarkerElement;
-  //endPointer?: SVGMarkerElement;
-  endPointer?: ArrowHead; //coudl use string as well
+  startPointer?: string;
+  endPointer?: string;
+  //no intermediate points and hence no intermediate markers now
 }
