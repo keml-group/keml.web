97a6a0442b0cddb0d3d581a24c451bc015b13db7
Susanne Göbel <goebel@uni-koblenz.de>
2025-03-30 12:48:39 +0200

    Move providers completely into app.config. This way it is available for app and modals. Adapted corresponding comment on the base service

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,15 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import {EditorComponent} from "@app/features/editor/features/editor/editor.component";
-import {ArrowStyleConfigurationService} from "@app/core/features/arrows/services/arrow-style-configuration.service";
-import {KEMLArrowStyleConfigurationService} from "@app/shared/keml/services/kemlarrow-style-configuration.service";
 
 
 @Component({
   selector: 'app-root',
   standalone: true,
   imports: [RouterOutlet, EditorComponent],
-  providers: [{ provide: ArrowStyleConfigurationService, useClass: KEMLArrowStyleConfigurationService }],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
--- src/app/core/features/arrows/services/arrow-style-configuration.service.ts
+++ src/app/core/features/arrows/services/arrow-style-configuration.service.ts
@@ -5,7 +5,7 @@ import {ArrowStyleConfiguration} from "@app/core/features/arrows/models/arrow-st
  default implementation
  You can extend this base case with an own service implementation.
  Make sure to configure DI to use your service instead of this one
- In 20205, the way to do so is to declare it as provider in app.component.ts
+ In 20205, the way to do so is to declare it as provider in app.config.ts or app.component.ts
  providers: [{ provide: ArrowStyleConfigurationService, useClass: YourServiceImplementation }],
  See https://angular.dev/guide/di/dependency-injection-providers
  ***/
