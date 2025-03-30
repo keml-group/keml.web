a9ea7a43dde365d32ab5a4485312ab26cd0b7443
Susanne Göbel <goebel@uni-koblenz.de>
2025-03-30 08:57:33 +0200

    Explanatory text about own arrow styling

--- src/app/core/features/arrows/services/arrow-style-configuration.service.ts
+++ src/app/core/features/arrows/services/arrow-style-configuration.service.ts
@@ -1,6 +1,14 @@
 import { Injectable } from '@angular/core';
 import {ArrowStyleConfiguration} from "@app/core/features/arrows/models/arrow-style-configuration";
 
+/*****
+ default implementation
+ You can extend this base case with an own service implementation.
+ Make sure to configure DI to use your service instead of this one
+ In 20205, the way to do so is to declare it as provider in app.component.ts
+ providers: [{ provide: ArrowStyleConfigurationService, useClass: YourServiceImplementation }],
+ See https://angular.dev/guide/di/dependency-injection-providers
+ ***/
 @Injectable({
   providedIn: 'root'
 })
