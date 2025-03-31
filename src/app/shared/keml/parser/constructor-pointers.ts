d1768b7ce4e773b0e2ca6f3194512f054dcb52ca
Susanne Göbel <goebel@uni-koblenz.de>
2025-03-31 09:34:35 +0200

    Remove now unused method, comment on map key. Actually, we could now remove this class in favor of a parser parameter

--- src/app/shared/keml/parser/constructor-pointers.ts
+++ src/app/shared/keml/parser/constructor-pointers.ts
@@ -1,10 +1,10 @@
-import  {Ref} from "@app/core/emfular/refs/ref"
 import {Referencable} from "@app/core/emfular/refs/referenceable";
 import {Parser} from "@app/shared/keml/parser/parser";
 
 
 export abstract class ConstructorPointers {
 
+  // key is the respective eClass
   constructorPointers: Map<string, (e:string) => (p: Parser) => Referencable >;
 
   protected constructor() {
@@ -13,15 +13,5 @@ export abstract class ConstructorPointers {
 
   mapGet(eClass:string): ((e: string) => ( parser: Parser ) => Referencable) | undefined  {
     return this.constructorPointers.get(eClass)
-  }
-
-  get<T extends Referencable>(ref: Ref, parser: Parser): T {
-    let constrPointer: ((e: string) => ( parser: Parser ) => Referencable) | undefined = this.constructorPointers.get(ref.eClass)
-    if (constrPointer) {
-      let constr = constrPointer(ref.$ref)
-      return (constr(parser) as T);
-    } else {
-      throw(`Constructor pointer for ${ref} not set.`);
-    }
   }
 }
