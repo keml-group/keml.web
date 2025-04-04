e11c8e4a3a46cfb9ced1fb567849543d4788510a
Susanne Göbel <goebel@uni-koblenz.de>
2025-04-04 15:27:27 +0200

    Use ngx-arrows1.0.1

--- src/app/shared/keml/components/helper/msg-inner/msg-inner.component.ts
+++ src/app/shared/keml/components/helper/msg-inner/msg-inner.component.ts
@@ -2,13 +2,13 @@ import {Component, Input, OnChanges, OnInit, SimpleChanges} from '@angular/core'
 import {Message} from "@app/shared/keml/models/core/msg-info";
 import { TextAreaSvgComponent } from '@app/core/features/svg-base-components/text-area-svg/text-area-svg.component';
 import { NgIf } from '@angular/common';
-import {ArrowSvgComponent} from "ngx-arrows";
+import {ArrowBetweenBoxesComponent} from "ngx-arrows";
 
 @Component({
     selector: '[msg-inner]',
     templateUrl: './msg-inner.component.svg',
     styleUrl: './msg-inner.component.css',
-    imports: [NgIf, ArrowSvgComponent, TextAreaSvgComponent]
+    imports: [NgIf, ArrowBetweenBoxesComponent, TextAreaSvgComponent]
 })
 export class MsgInnerComponent implements OnInit, OnChanges {
   @Input() msg!: Message;
