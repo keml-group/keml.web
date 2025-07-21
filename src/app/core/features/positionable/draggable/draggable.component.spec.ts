8bf75e14755915c062a2d40be3ea0556739747e5
Susanne Göbel <goebel@uni-koblenz.de>
2025-07-21 09:27:50 +0200

    Fix Draggable component test

--- src/app/core/features/positionable/draggable/draggable.component.spec.ts
+++ src/app/core/features/positionable/draggable/draggable.component.spec.ts
@@ -1,10 +1,19 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { DraggableComponent } from './draggable.component';
+import {Draggable} from "@app/core/features/positionable/positionable";
+import {SVGAccessService} from "ngx-arrows";
 
 describe('DraggableComponent', () => {
-  let component: DraggableComponent;
-  let fixture: ComponentFixture<DraggableComponent>;
+  let example = {gId: "id", position: { x: 1, y: 2, w: 2, h: 1 }};
+  let component: DraggableComponent<Draggable>;
+  let fixture: ComponentFixture<DraggableComponent<Draggable>>;
+
+  class DraggableTestComponent<T extends Draggable> extends DraggableComponent<T> {
+    constructor(svgAccessService: SVGAccessService) {
+      super(svgAccessService);
+    }
+  }
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
@@ -12,8 +21,9 @@ describe('DraggableComponent', () => {
     })
     .compileComponents();
 
-    fixture = TestBed.createComponent(DraggableComponent);
+    fixture = TestBed.createComponent(DraggableTestComponent<Draggable>);
     component = fixture.componentInstance;
+    component.elem = example;
     fixture.detectChanges();
   });
 
