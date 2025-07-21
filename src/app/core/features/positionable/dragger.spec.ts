1f3ebbbc21719508d30c64769c17e8a0165e5125
Susanne Göbel <goebel@uni-koblenz.de>
2025-07-21 09:29:32 +0200

    Fix Dragger test by example

--- src/app/core/features/positionable/dragger.spec.ts
+++ src/app/core/features/positionable/dragger.spec.ts
@@ -2,6 +2,8 @@ import { Dragger } from './dragger';
 
 describe('Dragger', () => {
   it('should create an instance', () => {
-    expect(new Dragger()).toBeTruthy();
+    let example = {gId: "id", position: { x: 1, y: 2, w: 2, h: 1 }};
+
+    expect(new Dragger(example)).toBeTruthy();
   });
 });
