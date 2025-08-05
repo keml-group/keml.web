460c28b402a4af356a7dadebdf50ee32dea4a29a
Susanne Göbel <goebel@uni-koblenz.de>
2025-08-05 09:29:54 +0200

    Add tests for text area component - it currently produces a too long first word

--- src/app/shared/keml/components/helper/svg-base-components/text-area-svg/text-area-svg.component.spec.ts
+++ src/app/shared/keml/components/helper/svg-base-components/text-area-svg/text-area-svg.component.spec.ts
@@ -8,9 +8,8 @@ describe('TextAreaSvgComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-    imports: [TextAreaSvgComponent]
-})
-    .compileComponents();
+      imports: [TextAreaSvgComponent]
+    }).compileComponents();
 
     fixture = TestBed.createComponent(TextAreaSvgComponent);
     component = fixture.componentInstance;
@@ -20,4 +19,50 @@ describe('TextAreaSvgComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  function initializeDistributedText() {
+    component.x = 20
+    component.y = 40
+    component.h = 50
+    component.w = 200
+
+  }
+
+  function verifyDistributedText(text: string, distributedText: string[]) {
+    component.text = text;
+    component.distributeText()
+    expect(component.distributedText).toEqual(distributedText)
+  }
+
+  it('Should set a two line distributed text', () => {
+    initializeDistributedText();
+
+    verifyDistributedText(
+      'Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam',
+      ['Lorem ipsum dolor sit', 'amet, consetetur...']
+    )
+  })
+
+  it('Should set a short distributed text after a longer one', () => {
+    initializeDistributedText();
+
+    verifyDistributedText(
+      'Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam',
+      ['Lorem ipsum dolor sit', 'amet, consetetur...']
+    ) //first set two lines, next set a shorter one
+    verifyDistributedText(
+      'Short',
+      ['Short']
+    )
+  })
+
+
+  it('Should handle a too long first word in a two line setting', () => {
+    initializeDistributedText();
+    verifyDistributedText(
+      'TooLongWordLongerLongerLongerLongerLonger, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam',
+      ['TooLongWordLongerLonger...', 'consetetur sadipscing...']
+    )
+  })
+
 });
