30696bc63df9bd2f03c0b53e2e9c69947b635ef0
Susanne Göbel <goebel@uni-koblenz.de>
2025-08-20 08:45:40 +0200

    Trust Slider moved to general level on shared

--- src/app/features/simulator/components/simulation-input-details/simulation-input-details.component.ts
+++ src/app/features/simulator/components/simulation-input-details/simulation-input-details.component.ts
@@ -6,7 +6,7 @@ import {SimulationInputs} from "@app/features/simulator/models/simulation-inputs
 import {NgForOf} from "@angular/common";
 import {ConversationPartner} from "@app/shared/keml/core/conversation-partner";
 import {TrustComputationService} from "@app/features/simulator/services/trust-computation.service";
-import {TrustSliderComponent} from "@app/shared/keml/graphical/helper/trust/trust-slider/trust-slider.component";
+import {TrustSliderComponent} from "@app/shared/trust-slider/trust-slider.component";
 
 @Component({
     selector: 'simulation-input-details',
