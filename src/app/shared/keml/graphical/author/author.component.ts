1e7c08fdf35f4601aa90c9a080129eb520224da2
Susanne Göbel <goebel@uni-koblenz.de>
2025-08-22 14:45:27 +0200

    Also use shorter lifeline mechanic on author

--- src/app/shared/keml/graphical/author/author.component.ts
+++ src/app/shared/keml/graphical/author/author.component.ts
@@ -1,7 +1,9 @@
-import {Component, Input} from '@angular/core';
+import {Component, computed, Input, Signal} from '@angular/core';
 import {Author} from "@app/shared/keml/core/author";
 import { TextAreaSvgComponent } from "ngx-svg-graphics";
 import { PersonSvgComponent } from '@app/shared/keml/graphical/helper/base-svg/person-svg/person-svg.component';
+import {KemlService} from "@app/shared/keml/core/keml.service";
+import {LayoutingService} from "@app/shared/keml/graphical/layouting.service";
 
 @Component({
     selector: '[authorG]',
@@ -13,5 +15,13 @@ export class AuthorComponent {
   //author has fixed position and no way to change name (right now)
   @Input() author!: Author;
 
+  lineLength: Signal<number> = computed(() =>
+    this.layoutingService.determineLifeLineLength(this.kemlService.msgCount()))
+
+  constructor(
+    private kemlService: KemlService,
+    private layoutingService: LayoutingService,
+  ) {
+  }
 
 }
