978d21eecafc4e28b4a6819c158481ebe1112434
Susanne Göbel <goebel@uni-koblenz.de>
2025-08-25 11:42:55 +0200

    Use conversation component also on simulator component

--- src/app/features/simulator/simulator/simulator.component.ts
+++ src/app/features/simulator/simulator/simulator.component.ts
@@ -1,40 +1,26 @@
 import {Component, ElementRef, Input, OnInit, ViewChild} from '@angular/core';
-import {NgForOf, NgIf, NgTemplateOutlet} from "@angular/common";
 import {MatIcon} from "@angular/material/icon";
 import {MatDialogRef} from "@angular/material/dialog";
 import {Conversation} from "@app/shared/keml/core/conversation";
-import {AuthorComponent} from "@app/shared/keml/graphical/author/author.component";
-import {ConversationPartnerComponent} from "@app/shared/keml/graphical/cp/conversation-partner.component";
-import {MsgComponent} from "@app/shared/keml/graphical/msg/msg.component";
-import {PreknowledgeComponent} from "@app/shared/keml/graphical/preknowledge/preknowledge.component";
-import {TextAreaSvgComponent} from "ngx-svg-graphics";
 import {MatToolbar} from "@angular/material/toolbar";
 import {TrustComputationService} from "../trust-computation/trust-computation.service";
 import {TrustFallbacks} from "@app/features/simulator/trust-computation/trust-fallbacks";
 import {IncrementalSimulationService} from "@app/features/simulator/incremental-simulation.service";
-import {ArrowMarkersComponent} from "@app/shared/keml/graphical/helper/arrow-styling/arrow-markers/arrow-markers.component";
 import {AlertService, IoService} from "ngx-emfular-helper";
 import {
   TrustFallbackControls
 } from "@app/features/simulator/trust-computation/trust-fallback-controls/trust-fallback-controls.component";
 import {InfoTrustDetailsService} from "@app/features/simulator/info-trust-details/info-trust-details.service";
+import {ConversationComponent} from "@app/shared/keml/graphical/conversation/conversation.component";
 
 @Component({
     selector: 'app-simulator',
   imports: [
     MatIcon,
-    AuthorComponent,
-    ConversationPartnerComponent,
-    MsgComponent,
-    NgForOf,
-    PreknowledgeComponent,
-    TextAreaSvgComponent,
     MatToolbar,
-    NgTemplateOutlet,
-    NgIf,
-    ArrowMarkersComponent,
     TrustFallbackControls,
-    TrustFallbackControls
+    TrustFallbackControls,
+    ConversationComponent
   ],
   providers: [IncrementalSimulationService],
     templateUrl: './simulator.component.html',
