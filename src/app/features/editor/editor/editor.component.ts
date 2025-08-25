628436a52a1c651d3f2556fd1f9e08c58698ba01
Susanne Göbel <goebel@uni-koblenz.de>
2025-08-25 11:34:46 +0200

    Use new conversation commponent on editor

--- src/app/features/editor/editor/editor.component.ts
+++ src/app/features/editor/editor/editor.component.ts
@@ -1,29 +1,23 @@
 import {Component, ElementRef, ViewChild,} from '@angular/core';
-import { NgFor } from '@angular/common';
 import { MatIcon } from '@angular/material/icon';
 import { MatToolbar } from '@angular/material/toolbar';
 import { MatTooltipModule } from "@angular/material/tooltip";
 import { KemlService } from "@app/shared/keml/core/keml.service";
 import { DetailsService } from "@app/features/editor/details/details.service";
-import { MsgComponent } from '@app/shared/keml/graphical/msg/msg.component';
-import { PreknowledgeComponent } from '@app/shared/keml/graphical/preknowledge/preknowledge.component';
-import { ConversationPartnerComponent } from '@app/shared/keml/graphical/cp/conversation-partner.component';
-import { AuthorComponent } from '@app/shared/keml/graphical/author/author.component';
-import { TextAreaSvgComponent } from "ngx-svg-graphics";
 import { DatabaseSvgComponent } from '@app/shared/keml/graphical/helper/base-svg/database-svg/database-svg.component';
 import { PersonSvgComponent } from '@app/shared/keml/graphical/helper/base-svg/person-svg/person-svg.component';
 import {SimulationDialogueService} from "@app/features/simulator/simulation-dialogue.service";
-import {ArrowMarkersComponent} from "@app/shared/keml/graphical/helper/arrow-styling/arrow-markers/arrow-markers.component";
 import {KEMLIOService} from "@app/features/editor/keml-io.service";
 import {ConversationPickService} from "@app/features/fromLLM/chatGPT2llm/conversationpicking/conversation-pick.service";
 import {IoService, InputHandler} from "ngx-emfular-helper";
+import {ConversationComponent} from "@app/shared/keml/graphical/conversation/conversation.component";
 
 
 @Component({
     selector: 'keml-editor',
     templateUrl: './editor.component.html',
     styleUrl: './editor.component.css',
-    imports: [MatTooltipModule, MatToolbar, MatIcon, PersonSvgComponent, DatabaseSvgComponent, TextAreaSvgComponent, AuthorComponent, NgFor, ConversationPartnerComponent, PreknowledgeComponent, MsgComponent, ArrowMarkersComponent]
+  imports: [MatTooltipModule, MatToolbar, MatIcon, PersonSvgComponent, DatabaseSvgComponent, ConversationComponent]
 })
 export class EditorComponent {
 
