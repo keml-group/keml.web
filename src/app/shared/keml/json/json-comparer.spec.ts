81f515c0e0a8a673a74bc35f87182830cd4a276f
Susanne Göbel <goebel@uni-koblenz.de>
2025-10-06 17:26:08 +0200

    Tests for json comparer

--- src/app/shared/keml/json/json-comparer.spec.ts
+++ src/app/shared/keml/json/json-comparer.spec.ts
@@ -4,4 +4,35 @@ describe('JsonComparer', () => {
   it('should create an instance', () => {
     expect(new JsonComparer()).toBeTruthy();
   });
+
+  it('should match two empty jsons', () => {
+    expect(JsonComparer.compare({}, {})
+      .isLessEquals()).toEqual(true)
+  })
+
+  it('should match two string lists (no $refs)', () => {
+    expect(JsonComparer.compare(['a', 'b'], ['a', 'b'])
+      .isLessEquals()).toEqual(true)
+  })
+
+  it('should NOT match two reordered string lists (no $refs)', () => {
+    expect(JsonComparer.compare(['a', 'b'], ['b', 'a'])
+      .isLessEquals()).toEqual(false)
+  })
+
+  it('should NOT match two reordered object lists (no $refs)', () => {
+    expect(JsonComparer.compare([{x: 'a'}, {x: 'b'}], [{x: 'b'}, {x:'a'}])
+      .isLessEquals()).toEqual(false)
+  })
+
+  it('should match two reordered object lists with $refs)', () => {
+    let res = JsonComparer.compare({x: [{$ref: 'a'}, {$ref: 'b'}]}, {x: [{$ref: 'b'}, {$ref:'a'}]});
+      expect(res.isLessEquals()).toEqual(true)
+  })
+
+  //todo
+  it('should NOT match two reordered object lists with $refs)', () => {
+    expect(JsonComparer.compare([{$ref: 'a'}, {$ref: 'b'}], [{$ref: 'b'}, {$ref:'a'}])
+      .isLessEquals()).toEqual(false)
+  })
 });
