cbee646aeb2e12da2e19e5567fdb3e26df75d527
Susanne Göbel <goebel@uni-koblenz.de>
2025-11-24 12:57:03 +0100

    Remove unused deserializer on constructor

--- src/app/shared/keml/core/conversation-partner.ts
+++ src/app/shared/keml/core/conversation-partner.ts
@@ -6,7 +6,7 @@ import {RefHandler} from "emfular";
 
 export class ConversationPartner extends LifeLine {
 
-    constructor(name: string = 'NewPartner', xPosition?: number, ref?: Ref, deserializer?: Deserializer) {
+    constructor(name: string = 'NewPartner', xPosition?: number, ref?: Ref) {
       let refC = RefHandler.createRefIfMissing(EClasses.ConversationPartner, ref)
       super(name, xPosition, refC);
     }
