11ffd0e8b0a5e8defc3cac6e1950db76e393775c
Susanne Göbel <goebel@uni-koblenz.de>
2025-11-24 15:42:13 +0100

    Remove unnecessary extra structure classes

--- src/app/shared/keml/structure/conversation-structure.spec.ts
+++
@@ -1,7 +0,0 @@
-import { ConversationStructure } from './conversation-structure';
-
-describe('ConversationStructure', () => {
-  it('should create an instance', () => {
-    expect(new ConversationStructure()).toBeTruthy();
-  });
-});
--- src/app/shared/keml/structure/reference-structure.spec.ts
+++
@@ -1,7 +0,0 @@
-import { ReferenceStructure } from './reference-structure';
-
-describe('ReferenceStructure', () => {
-  it('should create an instance', () => {
-    expect(new ReferenceStructure('a')).toBeTruthy();
-  });
-});
