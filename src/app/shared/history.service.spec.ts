5643aa038db1319a7d24f16c711f76bc4876ef5c
Susanne Göbel <goebel@uni-koblenz.de>
2025-11-26 08:38:01 +0100

    Test for save and undo

--- src/app/shared/history.service.spec.ts
+++ src/app/shared/history.service.spec.ts
@@ -1,9 +1,9 @@
 import { HistoryService } from './history.service';
-import {Observable} from "rxjs";
+import {fakeAsync, tick} from "@angular/core/testing";
 
-interface HistoryTester {value: string}
 
 describe('HistoryService', () => {
+  interface HistoryTester {inside: string}
   let prefix = "TestHistory_"
   let service = new HistoryService<HistoryTester>(prefix);
 
@@ -11,21 +11,33 @@ describe('HistoryService', () => {
     expect(service).toBeTruthy();
   })
 
-  it('should handle situations from a clear cache', () => {
+  it('should handle situations from a clear cache', fakeAsync(() => {
     localStorage.clear()
     let emitted: HistoryTester | null = null;
-    service.state$.subscribe(value => emitted = value);
+    service.state$.subscribe((value: HistoryTester | null) => emitted = value);
 
     service.init()
 
-    service.save({value: "s0"})
-    service.save({value: "s1"})
-    service.save({value: "s2"})
+    service.save({inside: "s0"})
+    service.save({inside: "s1"})
+    service.save({inside: "s2"})
 
+    tick();
     expect(emitted).toEqual(null)
     expect(localStorage.getItem(service.oldestEntryName)).toEqual("0")
     expect(localStorage.getItem(service.currentEntryName)).toEqual("2")
     expect(localStorage.getItem(service.newestEntryName)).toEqual("2")
 
-  });
+    service.undo()
+    tick()
+    expect(emitted).not.toBeNull();
+    expect((emitted! as HistoryTester).inside).toEqual('s1');
+    expect(localStorage.getItem(service.oldestEntryName)).toEqual("0")
+    expect(localStorage.getItem(service.currentEntryName)).toEqual("1")
+    expect(localStorage.getItem(service.newestEntryName)).toEqual("2")
+
+    
+
+
+  }))
 });
