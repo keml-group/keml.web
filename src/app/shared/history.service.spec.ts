c08ab483d86d674d02df8c8c40bc44cfec92e05c
Susanne Göbel <goebel@uni-koblenz.de>
2025-11-26 08:54:24 +0100

    Tests for undo redo sequences and one explicitly for invalidating too young entries if a new save hits in an intermediate state (undo and redo present)

--- src/app/shared/history.service.spec.ts
+++ src/app/shared/history.service.spec.ts
@@ -7,18 +7,31 @@ describe('HistoryService', () => {
   let prefix = "TestHistory_"
   let service = new HistoryService<HistoryTester>(prefix);
 
+  beforeEach(() => {
+    localStorage.clear()
+  })
+
   it('should be created', () => {
     expect(service).toBeTruthy();
   })
 
-  it('should handle situations from a clear cache', fakeAsync(() => {
-    localStorage.clear()
+  it('should handle saves, undos and redos from a clear cache', fakeAsync(() => {
     let emitted: HistoryTester | null = null;
     service.state$.subscribe((value: HistoryTester | null) => emitted = value);
 
     service.init()
 
+    expect(localStorage.getItem(service.oldestEntryName)).toEqual("-1")
+    expect(localStorage.getItem(service.currentEntryName)).toEqual("-1")
+    expect(localStorage.getItem(service.newestEntryName)).toEqual("-1")
+
+
     service.save({inside: "s0"})
+    expect(localStorage.getItem(service.oldestEntryName)).toEqual("0")
+    expect(localStorage.getItem(service.currentEntryName)).toEqual("0")
+    expect(localStorage.getItem(service.newestEntryName)).toEqual("0")
+
+
     service.save({inside: "s1"})
     service.save({inside: "s2"})
 
@@ -36,7 +49,63 @@ describe('HistoryService', () => {
     expect(localStorage.getItem(service.currentEntryName)).toEqual("1")
     expect(localStorage.getItem(service.newestEntryName)).toEqual("2")
 
-    
+
+    service.undo()
+    tick()
+    expect(emitted).not.toBeNull();
+    expect((emitted! as HistoryTester).inside).toEqual('s0');
+    expect(localStorage.getItem(service.oldestEntryName)).toEqual("0")
+    expect(localStorage.getItem(service.currentEntryName)).toEqual("0")
+    expect(localStorage.getItem(service.newestEntryName)).toEqual("2")
+
+    /* would require an emit even if no op
+    service.undo()
+    tick()
+    expect(emitted).toBeNull();
+    expect(localStorage.getItem(service.oldestEntryName)).toEqual("0")
+    expect(localStorage.getItem(service.currentEntryName)).toEqual("0")
+    expect(localStorage.getItem(service.newestEntryName)).toEqual("2")
+    */
+
+    service.redo()
+    tick()
+    expect(emitted).not.toBeNull();
+    expect((emitted! as HistoryTester).inside).toEqual('s1');
+    expect(localStorage.getItem(service.oldestEntryName)).toEqual("0")
+    expect(localStorage.getItem(service.currentEntryName)).toEqual("1")
+    expect(localStorage.getItem(service.newestEntryName)).toEqual("2")
+
+
+  }))
+
+  it('should remove newer elements when saving in an intermediate situation', fakeAsync(() => {
+    let emitted: HistoryTester | null = null;
+    service.state$.subscribe((value: HistoryTester | null) => emitted = value);
+
+    service.init()
+
+    service.save({inside: "s0"})
+    service.save({inside: "s1"})
+    service.save({inside: "s2"})
+    service.save({inside: "s3"})
+
+    service.undo()
+    service.undo()
+    tick()
+    expect(emitted!.inside).toEqual("s1")
+    expect(localStorage.getItem(service.oldestEntryName)).toEqual("0")
+    expect(localStorage.getItem(service.currentEntryName)).toEqual("1")
+    expect(localStorage.getItem(service.newestEntryName)).toEqual("3")
+
+    service.save({inside: "s4"})
+    expect(localStorage.getItem(service.oldestEntryName)).toEqual("0")
+    expect(localStorage.getItem(service.currentEntryName)).toEqual("2")
+    expect(localStorage.getItem(service.newestEntryName)).toEqual("2")
+
+    expect(localStorage.getItem(prefix+"2")).toEqual(JSON.stringify({inside: "s4"}))
+    expect(localStorage.getItem(prefix+"1")).toEqual(JSON.stringify({inside: "s1"}))
+    expect(localStorage.getItem(prefix+"0")).toEqual(JSON.stringify({inside: "s0"}))
+    expect(localStorage.getItem(prefix+"3")).toBeNull()
 
 
   }))
