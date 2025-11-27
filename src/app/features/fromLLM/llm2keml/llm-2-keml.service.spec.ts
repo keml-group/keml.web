8bba792c08efec781e6d78a3a9f8143a0402156b
Susanne Göbel <goebel@uni-koblenz.de>
2025-11-27 09:37:57 +0100

    Verify right model contetnts on keml after call from LLm service

--- src/app/features/fromLLM/llm2keml/llm-2-keml.service.spec.ts
+++ src/app/features/fromLLM/llm2keml/llm-2-keml.service.spec.ts
@@ -1,9 +1,9 @@
-import { Llm2KemlService } from './llm-2-keml.service';
+import {Llm2KemlService} from './llm-2-keml.service';
 import {TestBed} from "@angular/core/testing";
 import {KemlService} from "@app/shared/keml/edit/keml.service";
 import {KemlHistoryService} from "@app/shared/keml/edit/keml-history.service";
-import {BehaviorSubject, of} from "rxjs";
-import {ConversationJson} from "@app/shared/keml/json/sequence-diagram-models";
+import {of} from "rxjs";
+import {LLMMessageAuthorType} from "@app/features/fromLLM/llm2keml/llm.models";
 
 describe('LlmConversationCreationService', () => {
   let service: Llm2KemlService;
@@ -18,7 +18,7 @@ describe('LlmConversationCreationService', () => {
   });
 });
 
-describe('LlmService integration', () => {
+describe('LlmService integration with history', () => {
   let llmService: Llm2KemlService;
   let kemlService: KemlService;
   let historyStub: any;
@@ -43,11 +43,44 @@ describe('LlmService integration', () => {
     kemlService = TestBed.inject(KemlService);
   });
 
-  it('should trigger a single save when processing input', () => {
+  it('should trigger a single save when processing (empty) input', () => {
     llmService.convFromLlmMessages([]);
 
     expect(historyStub.save).toHaveBeenCalledTimes(1);
     expect(historyStub.save).toHaveBeenCalledWith(kemlService.serializeConversation());
+  });
+
+  it('should trigger a single save when processing real input', () => {
+    llmService.convFromLlmMessages([
+      {
+        author: LLMMessageAuthorType.Author,
+        message: 'm1'
+      },
+      {
+        author: LLMMessageAuthorType.LLM,
+        message: 'm2'
+      },
+      {
+        author: LLMMessageAuthorType.Author,
+        message: 'm3'
+      }
+    ]);
+
+    expect(historyStub.save).toHaveBeenCalledTimes(1);
+    expect(historyStub.save).toHaveBeenCalledWith(kemlService.serializeConversation());
+
+    //verify that the right object was created:
+    expect(kemlService.conversation.conversationPartners[0].name).toBe('LLM')
+
+    expect(kemlService.conversation.author.messages.length).toBe(3);
+    expect(kemlService.conversation.author.messages[0].content).toBe("m1");
+    expect(kemlService.conversation.author.messages[1].content).toBe("m2");
+    expect(kemlService.conversation.author.messages[2].content).toBe("m3");
+
+    expect(kemlService.conversation.author.messages[0].isSend()).toBeTrue();
+    expect(kemlService.conversation.author.messages[1].isSend()).toBeFalse();
+    expect(kemlService.conversation.author.messages[2].isSend()).toBeTrue();
+
   });
 });
 
