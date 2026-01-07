66b13008b2530dbc1d2e7c15f925e9c5bdc74a60
Susanne Göbel <goebel@uni-koblenz.de>
2026-01-07 09:46:07 +0100

    Hide sidebar and learn more texts depending on sidebar status

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -15,7 +15,17 @@ export class AppComponent {
 
   hideAside: boolean = false;
 
+  readonly textWhenShown: string = "Hide Sidebar";
+  readonly textWhenHidden: string = "Learn more";
+
+  text: string = this.textWhenShown;
+
   toggleAside() {
     this.hideAside = !this.hideAside;
+    this.decideText()
+  }
+
+  decideText() {
+    this.text = this.hideAside ? this.textWhenHidden : this.textWhenShown;
   }
 }
