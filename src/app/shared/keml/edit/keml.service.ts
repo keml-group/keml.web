8ceae5b0d34f59f7394ed9fd051b93559f00ea1f
Susanne Göbel <goebel@uni-koblenz.de>
2026-02-27 11:08:14 +0100

    Fix: make _conversation private

--- src/app/shared/keml/edit/keml.service.ts
+++ src/app/shared/keml/edit/keml.service.ts
@@ -23,7 +23,7 @@ import {KemlHistoryService} from "@app/shared/keml/edit/keml-history.service";
 })
 export class KemlService {
 
-  public _conversation!: Conversation;
+  private _conversation!: Conversation;
   get conversation(): Conversation {
     return this._conversation;
   }
